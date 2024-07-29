7e3b21c1f53c8f704ad3d9a778800e5c129c7ab9
Julia Samol <j.samol@papers.ch>
2024-07-29 18:16:18 +0200

    Feat/android bindings

---
+++ android/build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+}
+
+subprojects {
+    group = "com.acurast.ipfs"
+    version = "1.0.0-beta01"
+}
